f785e68a6d92248badb967eaac8955992e5e77dc
Plovatto <109766672+Plovatto@users.noreply.github.com>
2023-09-30 17:29:24 -0300

    Delete src directory

--- src/router/route.js
+++
@@ -1,8 +0,0 @@
-const express=require('express');
-const router = express.Router();
-
-router.get('/', (req, res, next)=>{
-  res.status(200).send("<h1>API para Quest Bank</h1>");
-})
-
-module.exports=router;
